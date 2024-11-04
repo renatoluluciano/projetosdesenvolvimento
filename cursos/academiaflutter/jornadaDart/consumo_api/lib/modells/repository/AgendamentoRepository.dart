8ee47745970c246b01da90d5dbc41af6d1b09654
renatoluluciano <renatolucianosilva@gmail.com>
2024-11-04 10:26:55 -0300

    Metodos no repositoryAgendamento buscar todos e buscar por id

--- cursos/academiaflutter/jornadaDart/consumo_api/lib/modells/repository/AgendamentoRepository.dart
+++ cursos/academiaflutter/jornadaDart/consumo_api/lib/modells/repository/AgendamentoRepository.dart
@@ -1,1 +1,33 @@
-class Agendamentorepository {}
+import 'dart:convert';
+
+import 'package:consumo_api/modells/Agendamento.dart';
+import 'package:http/http.dart' as http;
+
+class Agendamentorepository {
+  Future<List<Agendamento>> buscarTodos() async {
+    final agendamentosResponse =
+        await http.get(Uri.parse('http://localhost:3080/agendamentos'));
+
+    //Converter os dados para um Modelo
+
+    final agendamentosList = jsonDecode(agendamentosResponse.body);
+
+    final todosAgendamentos =
+        agendamentosList.map<Agendamento>((agendamentoMap) {
+      return Agendamento.fromMap(agendamentoMap);
+    }).toList();
+
+    // return agendamentosList.
+    //map<Agendamento>((agendamentoMap) => Agendamento.
+    //fromMap(agendamentoMap)).toString();
+
+    return todosAgendamentos;
+  }
+
+  Future<Agendamento> buscarId(int id) async {
+    final agendamentosId =
+        await http.get(Uri.parse('http://localhost:3080/agendamentos/$id'));
+
+    return Agendamento.fromJson(agendamentosId.body);
+  }
+}
