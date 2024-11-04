4065555a10d518e530c192a2dc6351545363dc5a
renatoluluciano <renatolucianosilva@gmail.com>
2024-11-04 09:08:26 -0300

    serializacao e deserializacao jason em dart

---
+++ cursos/academiaflutter/jornadaDart/consumo_api/lib/modells/Agendamento.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+class Agendamento {
+  Agendamento({
+    required this.id,
+    required this.nomePolicial,
+    required this.dataAfastamento,
+    required this.tipoAfastamento,
+  });
+
+  int id;
+  String nomePolicial;
+  DateTime dataAfastamento;
+  String tipoAfastamento;
+
+  //transforma o Map em uma instancia em um objeto
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "nomePolicial": nomePolicial,
+      "dataAfastamento": dataAfastamento,
+      "tipoAfastamento": tipoAfastamento
+    };
+  }
+
+  String toJson() => jsonEncode(toMap());
+  //to Json map de chave e valor e transforma em um json
+
+  factory Agendamento.fromMap(Map<String, dynamic> map) {
+    return Agendamento(
+        id: map['id'] ?? 0,
+        nomePolicial: map['nomePolicial'] ?? '',
+        dataAfastamento: map['dataAfastamento'] ?? '',
+        tipoAfastamento: map['tipoAfastamento'] ?? '');
+  }
+
+  factory Agendamento.fromJson(String json) =>
+      Agendamento.fromMap(jsonDecode(json));
+}
