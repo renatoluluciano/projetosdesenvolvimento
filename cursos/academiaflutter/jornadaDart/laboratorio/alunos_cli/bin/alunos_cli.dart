9898ad6990af7161aaff5c0820206669e3232fb3
renatoluluciano <renatolucianosilva@gmail.com>
2024-11-04 16:06:45 -0300

    Criacão do projeto laboratorio academia do flutter

---
+++ cursos/academiaflutter/jornadaDart/laboratorio/alunos_cli/bin/alunos_cli.dart
@@ -0,0 +1,5 @@
+import 'package:alunos_cli/alunos_cli.dart' as alunos_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${alunos_cli.calculate()}!');
+}
---
+++ cursos/academiaflutter/jornadaDart/laboratorio/alunos_cli/test/alunos_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:alunos_cli/alunos_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
