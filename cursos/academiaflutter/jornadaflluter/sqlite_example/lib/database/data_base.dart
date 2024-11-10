fc15b4eca14a69c4b058cf369d57fc7124d4809c
renatoluluciano <renatolucianosilva@gmail.com>
2024-11-10 15:35:35 -0300

    controle de versão e desativar o backp automatino no androi manifest alowwbackp e fullbackup

--- cursos/academiaflutter/jornadaflluter/sqlite_example/lib/database/data_base.dart
+++ cursos/academiaflutter/jornadaflluter/sqlite_example/lib/database/data_base.dart
@@ -10,10 +10,14 @@ class DataBaseSqLite {
     //estruturação do banco de dados SQLITE
     await openDatabase(
       dataBaseFinalLite,
-      version: 1,
+      version: 2,
+      onConfigure: (db) async {
+        await db.execute('PRAGMA foreign_keys = ON');
+      },
       //chamado somente no momento de criação do banco de Dados
-      //primeira vez que carrega o aplicativo
+
       onCreate: (Database db, int version) {
+        print('acessando on creat');
         final batch = db.batch();
 
         batch.execute('''
@@ -22,11 +26,62 @@ class DataBaseSqLite {
           nome varchar(200)
           )
              ''');
+
+        batch.execute('''
+          create table produto(
+          id Integer primary key autoincrement,
+          nome varchar(200)
+          )
+             ''');
+
+        // batch.execute('''
+        //   create table categoria(
+        //   id Integer primary key autoincrement,
+        //   nome varchar(200)
+        //   )
+        //      ''');
+
+        batch.commit();
       },
       //chamado sempre que houver uma alteracao no version, incremental
-      onUpgrade: (Database db, int oldVersion, int version) {},
+      onUpgrade: (Database db, int oldVersion, int version) {
+        final batch = db.batch();
+
+        if (oldVersion == 1) {
+          batch.execute('''
+          create table produto(
+          id Integer primary key autoincrement,
+          nome varchar(200)
+          )
+             ''');
+        }
+
+        // if (oldVersion == 2) {
+        //   batch.execute('''
+        //   create table categoria(
+        //   id Integer primary key autoincrement,
+        //   nome varchar(200)
+        //   )
+        //      ''');
+        // }
+
+        batch.commit();
+      },
       //chamando sempre que houver uma modificacao no version para menos, decremental
-      onDowngrade: (Database db, int oldVersion, int version) {},
+      onDowngrade: (Database db, int oldVersion, int version) {
+        final batch = db.batch();
+
+        if (oldVersion == 3) {
+          batch.execute('''
+
+            drop table categoria
+
+
+              ''');
+        }
+
+        batch.commit();
+      },
     );
   }
 }
